5003d8f337031a18707d50185116580784d47da6
curiosity26 <curiosity26@gmail.com>
2015-07-08 23:03:46 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,74 @@
+#Watchless#
+
+Unsatisfied with the less file watchers on NPM, I've created *watchless* to be a more reliable solution. 
+*Watchless* uses [https://www.npmjs.com/package/chokidar](**chokidar**) as the library for file watching which has better
+reliability than fs.watch or stalker.
+
+##Usage##
+
+###Command Line###
+
+```BASH
+
+Usage: watchless [options] <source> <destination>
+ 
+  <source>             =   The source directory or less file to compile
+  <destination>        =   The destination directory for the compiled CSS
+ 
+  OPTIONS
+     -e, --extension   =   The extension for the compiled CSS file, .min is prefixed if compression is enabled
+     -c, --compress    =   Enable compress on compiled CSS
+     -h, --help        =   This menu
+
+```
+
+In a typical watcher solution for package.json:
+
+*NOTE: run watchless last because it doesn't resume stdin on console, you have to press Ctrl+C to exit watching*
+
+```JSON
+...
+
+    {
+        "scripts": {
+            "watch-js": "watchify app/js/script.js -o app/static/bundle.min.js",
+            "watch-less": "watchless -c app/less app/css",
+            "watch": "npm run watch-js & npm run watch-less"
+        }
+    }
+...
+
+```
+
+```BASH
+npm run watch
+```
+
+###Node.js###
+
+```JS
+    var watchless = require("watchless");
+    
+    var options = {
+        source: "app/less/test.less",
+        destination: "app/css",
+        compress: true
+        };
+    
+    var watcher = watchless(options); // When source and destination are supplied in the options, watch starts automatically
+    
+```
+
+```JS
+    var watchless = require("watchless");
+    
+    var options = {
+        compress: true
+        };
+        
+    var watcher = watchless(options);
+    
+    watcher.watch("app/less", "app/css"); // The watcher can be started by calling watch. If the source or destination aren't supplied, they are pulled from the options
+    
+    watcher.stop(); // Stop watching files
+```
---
+++ watchless.js
@@ -0,0 +1,118 @@
+/**
+ * Created by alexboyce on 7/7/15.
+ */
+
+"use strict";
+
+var fs = require("fs"),
+    exec = require("child_process").exec,
+    chokidar = require("chokidar"),
+    extend = require("extend"),
+    path = require("path"),
+    less = require("less"),
+    watcher = null,
+    DEFAULT = {
+        source: null,
+        destination: null,
+        extension: '.css',
+    };
+
+function Watchless(opts) {
+
+    var obj = function(opts) {
+        var _this = this;
+
+        this.options = {};
+
+        this.watch = function (obj, dest) {
+            var sp = fs.realpathSync(obj || _this.options.source),
+                dp = dest || _this.options.destination;
+
+            if (!!watcher) {
+                watcher.stop();
+            }
+
+            var on_change = function(file) {
+                console.log("Change File: ", file);
+
+                if (!_this.options.destination) {
+                    throw new Error("Destination not defined.");
+                }
+                if (/\.less$/.test(file)) {
+                    fs.readFile(file, function(err, data) {
+                        if (err) {throw new Error(err);}
+
+                        var options = {
+                            paths         : [path.dirname(file)],
+                            outputDir     : _this.options.destination + "/",
+                            optimization  : 1,
+                            filename      : path.basename(file, '.less'),
+                            compress      : _this.options.compress,
+                            yuicompress   : _this.options.compress
+                        };
+
+                        options.outputfile = options.filename.split(".less")[0] + (options.compress ? ".min" : "") + _this.options.extension;
+
+                        less.render(data.toString(), options, function(err, result) {
+                            if (!err) {
+                                fs.writeFileSync(options.outputDir + options.outputfile, result.css, 'utf8' );
+                            }
+                        });
+                    });
+                }
+            };
+
+            fs.exists(sp, function(exists) {
+                if (exists) {
+                    _this.options.source = sp;
+
+                    fs.exists(dp, function(exists) {
+                        if (!exists) {
+                            fs.mkdirSync(dp);
+                        }
+
+                        _this.options.destination = fs.realpathSync(dp);
+
+                        watcher = chokidar.watch(sp, {ignored: /[\/\\]\./, persistent: true});
+                        watcher.on('change', on_change);
+                        watcher.on('add', on_change);
+                    });
+                }
+                else {
+                    throw new Error("File not found.");
+                }
+            });
+        };
+
+        this.stop = function() {
+            if (!!watcher) {
+                watcher.stop();
+            }
+        };
+
+        this.init = function (opts) {
+            if (!!watcher) {
+                watcher.close();
+            }
+
+            extend(_this.options, opts);
+
+            if (!!_this.options.source && !!_this.options.destination) {
+                _this.watch();
+            }
+        };
+
+        process.on('exit', function() {
+            if (!!watcher) {
+                watcher.close();
+            }
+        });
+
+        extend(this.options, DEFAULT);
+        this.init(opts);
+    };
+
+    return new obj(opts);
+}
+
+module.exports = Watchless;
